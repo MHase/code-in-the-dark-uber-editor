2a51f5cc08e0193d80f48c97527001cc5bb23309
Maciej Sykuła <maciek.sykula@gmail.com>
2019-08-14 09:33:44 +0200

    Setup new project with configs

---
+++ src/renderer/test.ts
@@ -0,0 +1,12 @@
+function greeter(person: string) {
+  return 'Hello, ' + person;
+}
+
+console.log(greeter('Maciej'));
+const temp = 'co';
+
+console.log(temp);
+
+console.log('test');
+
+exports.module = {};
