21b5aeef0e865a7e57c7ac6f06f76bf3d62fdc3b
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-08-19 15:52:38 +0200

    Formik - add styles

--- src/renderer/form.tsx
+++ src/renderer/form.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    btn: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
@@ -20,54 +35,72 @@ interface FormValues {
   url: string;
 }
 
-const handleSubmit = (values: FormValues, { setSubmitting } : { setSubmitting: Function }) => {
+const initialValues = {
+  name: window.localStorage.getItem('name') || '',
+  url: window.localStorage.getItem('url') || '',
+};
+
+const handleSubmit = (values: FormValues, { setSubmitting }: { setSubmitting: Function }) => {
   setTimeout(() => {
     alert(JSON.stringify(values, null, 2));
     setSubmitting(false);
   }, 400);
+  window.localStorage.setItem('name', values.name);
+  window.localStorage.setItem('url', values.url);
 };
 
-const WelcomeForm = () => (
-  <React.Fragment>
-    <Container maxWidth="sm">
-      <Formik initialValues={{ name: '', url: '' }} validationSchema={SignupSchema} onSubmit={handleSubmit}>
-        {({ values: { name, url }, errors, touched, isSubmitting, handleBlur, handleChange }) => {
-          return (
-            <Form>
-              <TextField
-                type="text"
-                name="name"
-                label="Name"
-                value={name}
-                error={touched.name && Boolean(errors.name)}
-                helperText={touched.name && errors.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                type="url"
-                name="url"
-                label="Url"
-                value={url}
-                error={touched.url && Boolean(errors.url)}
-                helperText={touched.url && errors.url}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                fullWidth
-                margin="normal"
-              />
+const WelcomeForm = () => {
+  const classes = useStyles();
 
-              <Button variant="contained" color="secondary" type="submit" disabled={isSubmitting}>
-                Start
-              </Button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </Container>
-  </React.Fragment>
-);
+  return (
+    <React.Fragment>
+      <Container maxWidth="sm" className={classes.container}>
+        <Formik initialValues={initialValues} validationSchema={SignupSchema} onSubmit={handleSubmit}>
+          {({ values: { name, url }, errors, touched, isSubmitting, handleBlur, handleChange }) => {
+            return (
+              <Form>
+                <TextField
+                  type="text"
+                  name="name"
+                  label="Name"
+                  value={name}
+                  error={touched.name && Boolean(errors.name)}
+                  helperText={touched.name && errors.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  type="url"
+                  name="url"
+                  label="Url"
+                  value={url}
+                  error={touched.url && Boolean(errors.url)}
+                  helperText={touched.url && errors.url}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  fullWidth
+                  margin="normal"
+                />
+
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  disabled={isSubmitting}
+                  size="large"
+                  className={classes.btn}
+                >
+                  Start
+                </Button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default WelcomeForm;
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import WelcomeForm from './form';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <WelcomeForm />
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <WelcomeForm />
+        </header>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
