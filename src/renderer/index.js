af0b44d5e734f1e589fffcf1df3ecb1ef25ea6e4
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-08-20 12:33:55 +0200

    Add mode switch

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import WelcomeForm from './form';
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-});
+const setTheme = type =>
+  createMuiTheme({
+    palette: {
+      type: type ? 'dark' : 'light',
+    },
+  });
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = React.useState(true);
+
+  const handleChange = event => {
+    setDarkTheme(event.target.checked);
+  };
+
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={setTheme(darkTheme)}>
       <CssBaseline />
       <div className="App">
+        <FormControlLabel
+          control={<Switch checked={darkTheme} onChange={handleChange} value="checkedDarkMode" />}
+          label="Dark mode"
+        />
         <header className="App-header">
           <WelcomeForm />
         </header>
