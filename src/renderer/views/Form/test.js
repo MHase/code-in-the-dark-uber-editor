78a744bd3d42dcc3e09048b5de6cb773add23232
Maciej Sykuła <maciek.sykula@gmail.com>
2019-08-23 13:35:36 +0200

    Add icon, fix undefined context wrapper in tests

--- src/renderer/views/Form/test.js
+++ src/renderer/views/Form/test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Form from './';
+import StorageProvider from '../../context/storage';
 
 describe('View | Form', () => {
   it('matches the snapshot', () => {
-    const container = render(<Form />);
+    const container = render(<Form />, { wrapper: StorageProvider });
     expect(container).toMatchSnapshot();
   });
 });
